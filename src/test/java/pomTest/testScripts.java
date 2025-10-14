26a38447efb26bd38300cf3759d0bbd320af2aed
gkiruthika15 <gkiruthika15@gmail.com>
2025-10-14 05:28:40 +0000

    Add automation scripts

--- src/test/java/pomTest/testScripts.java
+++ src/test/java/pomTest/testScripts.java
@@ -1,56 +1,47 @@
 package pomTest;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import org.testng.Assert;
-import org.testng.AssertJUnit;
-import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pomPages.HomePage;
+import pomPages.LoanCalculator;
+import static org.testng.Assert.assertTrue;
 
-import pomPages.*;
+public class testScripts {
+    WebDriver driver;
 
-public class testScripts{
-	WebDriver driver;
-	@BeforeClass
-	public void setup() {
-		driver = new ChromeDriver();
-		 
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-		driver.get("https://www.experian.com");
-		System.out.println("Navigating to url");
-		
-	}
-	@Test(priority=1)
-	public void homePage() {
-		HomePage home = new HomePage(driver);
-		home.clickLoanNav();
-		home.clickLoanCalculator();
-	}
-	
-	@Test(priority=2)
-	public void loanCalculation() throws Exception {
-		LoanCalculator loan = new LoanCalculator(driver);
-		loan.enterLoanAmount("10000");
-		loan.enterInterestRate("15");
-		loan.enterTerm("5");
-		loan.clickButton();
-		Thread.sleep(3000);
-		String actualText = loan.verifyResult();
-		
-		Assert.assertTrue(actualText.contains("Total estimated monthly payment"), "Result Text does not match!");
-		Thread.sleep(3000);
-	}
-	
-	@AfterClass
-	public void tearDown() {
-		driver.close();
-	}
+        driver.get("https://www.experian.com");
+    }
+
+    @Test(priority = 1)
+    public void homePage() {
+        HomePage home = new HomePage(driver);
+        home.clickLoanNav();
+        home.clickLoanCalculator();
+    }
+
+    @Test(priority = 2, dependsOnMethods = {"homePage"})
+    public void loanCalculation() throws InterruptedException {
+        LoanCalculator loanCalc = new LoanCalculator(driver);
+        loanCalc.enterLoanAmount("10000");
+        loanCalc.enterInterestRate("15");
+        loanCalc.enterTerm("5");
+        loanCalc.clickButton();
+        Thread.sleep(3000);
+        String result = loanCalc.verifyResult();
+        assertTrue(result.contains("Total estimated monthly payment"), "Result text validation failed");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
